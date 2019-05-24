093d4bfd9cd8135094056fbb0f5a02b2b1dd33e5
wangyan6@cmcm.com <wangyan6@cmcm.com>
2019-05-24 17:59:25 +0800

    init

---
+++ Assets/Scripts/WYBall.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WYBall : MonoBehaviour
+{
+    public float speed = 200f;
+
+    private void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "racket")
+        {
+            float x = hitFactor(transform.position, collision.transform.position, collision.collider.bounds.size.x);
+            Vector2 dir = new Vector2(x, 1).normalized;
+
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        }
+    }
+
+    private float hitFactor(Vector2 ballPos, Vector2 racketPos, float racketWidth)
+    {
+        return (ballPos.x - racketPos.x) / racketWidth;
+    }
+}
---
+++ Assets/Scripts/WYRacket.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WYRacket : MonoBehaviour
+{
+    public float speed = 150f;
+
+    private Vector2 lastMousePosition;
+    private float leftLimt = -87;
+    private float rightLimt = 87;
+
+    private void FixedUpdate()
+    {
+
+    }
+
+    private void OnGUI()
+    {
+        var currentPosition = Event.current.mousePosition;
+
+        if (Event.current.type == EventType.MouseDown)
+        {
+            lastMousePosition = currentPosition;
+            Debug.Log("[WY_Rocket] MouseDown");
+
+        } else if (Event.current.type == EventType.MouseUp)
+        {
+            Debug.Log("[WY_Rocket] MouseUp");
+        } else if (Event.current.type == EventType.MouseDrag)
+        {
+            var deltaX = currentPosition.x - lastMousePosition.x;
+            lastMousePosition = currentPosition;
+            Debug.Log("[WY_Rocket] MouseDrag deltaX:" + deltaX);
+
+            var x = transform.position.x + deltaX;
+            if (x < leftLimt)
+            {
+                x = leftLimt;
+            } else if (x > rightLimt)
+            {
+                x = rightLimt;
+            }
+
+            transform.position = new Vector3(x, transform.position.y, transform.position.z);
+        }
+    }
+}
